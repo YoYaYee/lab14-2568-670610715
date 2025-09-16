4b6480550ce08a778fd9f17805e80377a3bb37d3
Yoyayee <eden555007@gmail.com>
2025-09-16 17:13:26 +0700

    fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export default function Sidebar() {
             color="red"
             withBorder
           >
-            <Avatar size="md" radius="xl" src="/loma.jpeg" />
+            <Avatar size="md" radius="xl" src="/photo.jpeg" />
           </Indicator>
           <Text component={RouterNavLink} to="/">
             User : Pawornpruch : Student
